ecbdc09cdfd4295977b2962fa12e801a243d1648
Gabriel <gabriel.ferreira2002@hotmail.com>
2022-06-24 15:31:17 -0300

    requisito 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const GET_EMAIL = 'GET_EMAIL';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const getEmail = (email) => ({
   type: GET_EMAIL,
   email,
+});
+
+export const getCurrencies = (currencies) => ({
+  type: GET_CURRENCIES,
+  currencies,
 });
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import Header from '../Components/Header';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  state = {
+    currencies: [],
+  };
+
+  componentDidMount() {
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies = async () => {
+    const { dispatch } = this.props;
+
+    const url = 'https://economia.awesomeapi.com.br/json/all';
+    const response = await fetch(url);
+    const data = await response.json();
+    delete data.USDT;
+    const coinCodeList = Object.keys(data);
+    console.log(coinCodeList);
+
+    this.setState({
+      currencies: [coinCodeList],
+    });
+    dispatch(getCurrencies(coinCodeList));
+  };
+
   render() {
+    const { currencies } = this.state;
+    const { email } = this.props;
+
     return (
-      <div>
-        <Header />
-      </div>
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
